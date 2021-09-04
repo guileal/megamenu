f932ad384e94c7a6fca52339342113cfc8e160be
Guilherme Leal <guileal.dev@gmail.com>
2021-09-04 04:03:44 -0300

    last version before image hovers

--- megamenu.js
+++ megamenu.js
@@ -1,16 +1,18 @@
 const megaMenu = document.getElementById("megamenu-wrapper");
 const openMenu = document.getElementById("open-menu");
-const closeMenu = document.querySelectorAll("close-menu");
+const closeMenu = document.querySelectorAll(".close-button");
 const rect = openMenu.querySelectorAll("rect");
 openMenu.addEventListener("click", function () {
 	megaMenu.classList.toggle("megamenu-wrapper-active");
-
-	console.log(openMenu);
 	rect.forEach((rect) => {
 		rect.classList.toggle("open-menu-active");
 	});
 });
 
-closeMenu.addEventListener("click", function () {
+closeMenu.forEach(closeMenu => {
+	closeMenu.addEventListener("click", closeMegaMenu)
+})
+
+function closeMegaMenu(){
 	megaMenu.classList.remove("megamenu-wrapper-active");
-});
+}
